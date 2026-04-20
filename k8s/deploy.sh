f7093229a1ebdcbc42d88f47879457183c0fd278
AnasGalal99 <98519881+AnasGalal99@users.noreply.github.com>
2026-04-20 13:42:04 +0200

    Update deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -6,4 +6,5 @@ kubectl apply -f 00-namespace.yaml
 echo "Applying all resources..."
 kubectl apply -f .
 kubectl rollout restart deployment coredns -n kube-system
+kubectl delete pod -n kube-system -l k8s-app=kube-dns
 echo "Done!"
